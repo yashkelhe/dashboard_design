84907722ee3b689601bb6225681c6de541908706
yashkelhe <94190611+yashkelhe@users.noreply.github.com>
2023-10-29 15:37:06 +0530

    full project
    hello

---
+++ Admin Dashboard #01/orders.js
@@ -0,0 +1,20 @@
+const Orders = [
+  {
+    location: "bandra",
+    System_No: "85743",
+    paymentStatus: "Due",
+    status: "Pending",
+  },
+  {
+    location: "Churchgate",
+    System_No: "97245",
+    paymentStatus: "Refunded",
+    status: "Declined",
+  },
+  {
+    location: "Navi-Mumbai",
+    System_No: "36452",
+    paymentStatus: "Paid",
+    status: "Active",
+  },
+];
